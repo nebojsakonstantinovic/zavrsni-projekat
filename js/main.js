e9039348d65dc7761192adcfc17dfe865310fd90
Nebojsa Konstantinovic <nebojsa.konstantin@gmail.com>
2017-12-18 23:57:07 +0100

    problemi sa portfoliom i uradjen kod od skills bara + sitne popravke

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 $(document).ready(function () {
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    // kode od SKILLS bara
+
+    $(window).scroll(function () {
+        bar();
+    });
+    bar();
+
+    function bar() {
+
+        var wh = $(window).height();
+        var scroll = $(window).scrollTop();
+
+        $('.blue-bar').each(function () {
+            var position = $(this).offset().top;
+
+            if (position < wh + scroll - 100) {
+
+                $('.blue-bar').each(function () {
+                    var skill = $(this).attr('data-skill');
+                    $(this).find('.white-bar').css({'width': skill + '%'});
+                });
+            }
+        });
+    }
+
+
+
+
+
+
+
 });
